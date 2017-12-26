1a4d6d4a6f5ab441614165aed9ab994866d6af24
ArchanaHemukumar <archana.h@tringapps.com>
2017-12-26 14:54:20 +0530

    Initial commit

---
+++ app/src/main/java/com/example/tringappsadmin/butterknifesample/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.tringappsadmin.butterknifesample;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends Activity {
+    @BindView(R.id.textView)
+    TextView title;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        title.setText("Hello from Butterknife");
+    }
+
+    @OnClick(R.id.button)
+    public void submit() {
+        Toast.makeText(MainActivity.this,
+                "Hello from Butterknife OnClick annotation", Toast.LENGTH_SHORT).show();
+    }
+
+}
